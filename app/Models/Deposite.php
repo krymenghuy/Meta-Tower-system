286996e4fc8e0a378022b833cea03226df0a8d7b
Lylay-09 <pozzlay549@gmail.com>
2023-08-07 14:28:57 +0700

    deposite

--- app/Models/Deposite.php
+++ app/Models/Deposite.php
@@ -15,7 +15,7 @@ class Deposite //extends Model
             'campus_id' => '1|number|exists=campuses.id',
             'session_id' => '1|number|exists=sessions.id',
             'parent_phone' => '1|string|1,20',
-            'deposite_amount' => '1|number',
+            'amount' => '1|number',
             'date_of_birth' => '1|string',
             'expire_date' => '1|string',
             'note' => '0|string',
@@ -24,6 +24,10 @@ class Deposite //extends Model
         $res = validateObject($arr,$v_rule,0,[],$ss->lang,0,null);
         if($res->error) return DV::error($res->error);
         $inputs = $res->values;
+        $amount = $inputs['amount'];
+        unset($inputs['amount']);
+        $inputs['deposite_amount'] = $amount;
+        // $amount
 
         $inputs['status_id'] = 2; //** default 2 = authorized */
         $newID = saveData($ss,'deposite',['id' => $id],$inputs,[],1);
@@ -34,7 +38,7 @@ class Deposite //extends Model
         $branch_id = $ss->branch_id;
         $rows = DB::table('deposite as d')
                 ->join('program_levels as l','l.id','=','d.level_id')
-                ->selectRaw('d.status_id,d.student_name,d.id,l.name as level,d.parent_phone,d.expire_date,d.date_of_birth')
+                ->selectRaw('d.deposite_amount as amount,d.status_id,d.student_name,d.id,l.name as level,d.parent_phone,d.expire_date,d.date_of_birth')
                 ->where('d.branch_id',$branch_id)
                 ->get();
         foreach($rows as $row){
@@ -48,7 +52,7 @@ class Deposite //extends Model
         $branch_id = $ss->branch_id;
         $row = DB::table('deposite as d')
                 ->join('program_levels as l','l.id','=','d.level_id')
-                ->selectRaw('d.status_id,d.student_name,d.id,l.name as level,d.parent_phone')
+                ->selectRaw('d.deposite_amount as amount,d.status_id,d.student_name,d.id,l.name as level,d.parent_phone')
                 ->where('d.branch_id',$branch_id)
                 ->where('d.id',$id)
                 ->get()->first();
