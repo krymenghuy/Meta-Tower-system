4aa046c29077fd6cd5a4d94b22f1326933a521eb
vectorasoft <vectorasoft.dev@gmail.com>
2023-08-08 11:29:13 +0700

    update ui

--- resources/views/layouts/findStudentComponent.blade.php
+++ resources/views/layouts/findStudentComponent.blade.php
@@ -1,14 +1,14 @@
 <div id="_main_findStudentComponent" class="mobile-padding p-3" style="display:none">
-    <div class="div--ssp">
+    <div id="div--ssp" class="div--ssp">
         <div class="d-flex align-items-center justify-content-center bg-white rounded-4 p-3">
             <div class="d-block">
                 <h2 class="trans-text text-primary text-center" data-langprop="titles.Search Student for Payment"></h2>
                 <div class="d-bf d-flex aling-items-center mt-5 gap-2">
-                    <input type="search" class="form-control data-input width--select-inner" data-field="name_id" placeholder="Search by Name or ID..."/>
-                    <div class="width--select-inner">
-                        <select class="modal-select2 form-control data-input" data-field="year"></select>
+                    <input type="search" class="form-control data-input width--search-inner" data-field="search_value" placeholder="Search by Name or ID..."/>
+                    <div class="width--search-inner">
+                        <select class="modal-select2 form-control data-input" data-field="academic_year"></select>
                     </div>
-                    <button class="btn btn-primary btn--find" type="button">
+                    <button id="btn--find" class="btn btn-primary btn--find" type="button">
                         <i class="fa-solid fa-magnifying-glass"></i>
                         <span class="trans-text" data-langprop="buttons.Find"></span>
                     </button>
@@ -20,7 +20,7 @@
             <textarea class="form-control data-text" data-field="note"></textarea>
         </div>
     </div>
-    <div class="div--fsd" style="display:none">
+    <div id="div--fsd" class="div--fsd" style="display:none">
         <div class="d-flex gap-2">
             <button class="btn btn-primary btn-sm btn--select" type="button">
                 <span></span>
