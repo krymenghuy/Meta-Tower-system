4843692e4d11678bccc9c851fb09ff691fe2d3b8
vectorasoft <vectorasoft.dev@gmail.com>
2023-08-22 10:07:00 +0700

    update ui

--- public/js/components/PromoteStudentComponent.js
+++ public/js/components/PromoteStudentComponent.js
@@ -4,7 +4,18 @@ var PromoteStudentComponent = new function(){
     this.title_prop = 'Promote';
     this.self = $('#_main_promoteStudentComponent');
 
-    this.init = () => {}
+    this.btnNew = mThis.self.find('#_pms_btn_new');
+
+    this.init = () => {
+        mThis.btnNew.on('click',function(e){
+            e.preventDefault();
+            let op = {
+                'id': 0,
+                'onClose': () => {}
+            };
+            PromoteStudentDialog.show(op);
+        });
+    }
 
     this.show = (options) => {
         if(!options) options = {};
@@ -12,6 +23,47 @@ var PromoteStudentComponent = new function(){
         let x = mThis.self.siblings(':visible');
         x.fadeOut('fast',function(){
             mThis.self.hide().fadeIn(300);
+        });
+    }
+}
+
+let PromoteStudentDialog = new function(){
+    let mThis = this;
+    this.self = $('#dlg_pms_');
+
+    this.prepareFormOption = (onFinish = null) => {
+        window.vsapi.call(`${main_view.base_url}/api/form-option`,null,null,false).then(res => {
+            let d = {};
+            if(res.status_code === 200){
+                d = res.data;
+            }
+            mThis.self.find('.data-input').each(function(){
+                let el = $(this);
+                let f = el.data('field');
+                switch(f){
+                    case 'term_id':
+                        VSUtil.setComboItems(el,d.terms,'id','term',null,null,null);
+                        break;
+                    case 'next_term_id':
+                        VSUtil.setComboItems(el,d.terms,'id','term',null,null,null);
+                        break;
+                    case 'program_id':
+                        VSUtil.setComboItems(el,d.programs,'id','program_name',null,null,null);
+                        break;
+                    default:
+                        break;
+                }
+            });
+            if(typeof onFinish === 'function') onFinish();
+        });
+    }
+
+    this.show = (options) => {
+        if(!options) options = {};
+        mThis.prepareFormOption(() => {
+            mThis.self.modal({
+                backdrop: 'static'
+            });
         });
     }
 }
--- resources/views/layouts/promoteStudentComponent.blade.php
+++ resources/views/layouts/promoteStudentComponent.blade.php
@@ -1,3 +1,65 @@
 <div id="_main_promoteStudentComponent" class="mobile-padding p-3" style="display:none">
-    <div class="d-flex gap-2"></div>
+    <div class="d-flex gap-2 bg-white rounded-3 p-3">
+        <button id="_pms_btn_new" class="btn btn-primary btn-sm" type="button">
+            <span class="trans-text text-nowrap" data-langprop="buttons.Promote Student"></span>
+        </button>
+        <div class="input-group flex-nowrap">
+            <input type="search" class="form-control width-search-box"/>
+            <div class="input-group-text">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+    <div class="table-responsive p-3 bg-white rounded-3 mt-3"></div>
+</div>
+
+<div class="modal fade" id="dlg_pms_" tabindex="-1" aria-labelledby="dlg_pms_title" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title trans-text" data-langprop="titles.Promote Student"></h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row gy-2">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="term_id" class="form-label trans-text" data-langprop="titles.Current Term"></label>
+                            <div class="width-select-dialog">
+                                <select class="modal-select2 data-input" data-field="term_id"></select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="next_term_id" class="form-label trans-text" data-langprop="titles.Next Term"></label>
+                            <div class="width-select-dialog">
+                                <select class="modal-select2 data-input" data-field="next_term_id" disabled></select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gy-2">
+                    <div class="col-lg-6">
+                        <label for="program_id" class="form-label trans-text" data-langprop="titles.Program"></label>
+                        <div class="width-select-dialog">
+                            <select class="modal-select2 data-input" data-field="program_id"></select>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="total_student" class="form-label trans-text" data-langprop="titles.Total Student"></label>
+                        <input type="text" class="form-control data-input" data-field="total_student" readonly/>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">
+                    <span class="trans-text" data-langprop="buttons.Cancel"></span>
+                </button>
+                <button type="button" class="btn btn-primary btn-sm">
+                    <span class="trans-text" data-langprop="buttons.Save"></span>
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
