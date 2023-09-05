1e0ba99d2bab6141d5f21ba58c9eae8fdefca204
Lylay-09 <pozzlay549@gmail.com>
2023-09-05 11:08:19 +0700

    mobile api

--- app/helpers.php
+++ app/helpers.php
@@ -1721,6 +1721,14 @@ function readFileContent($fileName=null)
 
         return (object)['end_date' => $date];
     }
+    function isExists($table,$pk=[],$checkCol,$inputValue){
+        $exists = DB::table($table)->where($pk)->selectRaw($checkCol)->first();
+        if($exists && $exists->$checkCol != $inputValue){
+            $exists = DB::table($table)->where($checkCol,$inputValue)->exists();
+            if($exists) return true;
+        }
+        return false;
+    }
 
 
 ?>
