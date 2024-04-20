ae838253ce5e8956ff515468ccce9ab7ba87364a
Krymengchhorng <mengchhorngk@gmail.com>
2024-04-20 16:21:25 +0700

    error

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Dms\Notifier;
+// use App\Models\Dms\Notifier;
+use App\Models\Notifier;
 use Illuminate\Http\Request;
 use App\Models\JDV;
 use App\Models\UM;
