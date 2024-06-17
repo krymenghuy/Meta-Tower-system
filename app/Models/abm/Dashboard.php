41402682471546949d90abeff34a6ad0a52384ed
rith-me <gbol7415@gmail.com>
2024-06-17 10:18:31 +0700

    get

--- app/Models/abm/Dashboard.php
+++ app/Models/abm/Dashboard.php
@@ -20,152 +20,8 @@ class Dashboard //extends Model
         $this->userInfo =$userInfo;
     }
     
-    function getSuplierList(){
-        // return JDV::result(DB::table('shipments')->selectRaw('zone_code,sender_id')->get());
-        return DB::table('os_suppliers')->selectRaw('id,name, phone_number, email, address,status_code,price_list_id,formatDate(create_date) as create_date,DATE_FORMAT(create_date,\'%r\') AS request_time')->get();
-    }
-
-    function checkUniquePerson($branch_id,$phone_number,$id=null){
-        $str_id ="1=1";
-        if(!$phone_number) return 'Phone number cannot be empty';
-        if ($id>0) $str_id="s.id <> $id";
-        $x = DB::table('os_suppliers as s')->where('s.branch_id',$branch_id)->where("s.phone_number",$phone_number)->whereRaw($str_id)->select('id')->take(1)->exists();
-        if ($x) return 'Phone number "'.$phone_number.'" is already save...';
-        return null;
-      }
-
-    function getOverseaItemList(){
-        // return JDV::result(DB::table('shipments')->selectRaw('zone_code,sender_id')->get());
-        return DB::table('oversea_items')->selectRaw('item_type, billed_weight, actual_weight, allocated_kg, heigth, weigth, length')->get();
-    }
-
-  
-
     
-    function getSuplierListPaginate($filter,$ss){
-        $branch_id = $ss->branch_id;
-        $d = (object)$filter;
-        // return JDV::result($filter->page);
-
-        $current_page = isset($d->current_page)?$d->current_page:1;
-        $per_page = isset($d->per_page)?$d->per_page:10;
-        $search_value = isset($d->search_value)?$d->search_value:null;
-        
-        $status_code = isset($d->status_code)?$d->status_code:null;
-        $price_list_id = isset($d->price_list_id)?$d->price_list_id:null;
-        $str_srch = '1=1';
-        $str_where = '2=2';
-        if($search_value){
-            $skip_row = 0;
-            $str_srch = '(s.name LIKE \'%'.$search_value.'%\')';
-        }
-        if($status_code){
-            $str_where = 's.status_code = \''.$status_code.'\'';
-        }
-        // if($price_list_id){
-        //     $str_where = 's.price_list_id = '.$price_list_id;
-        // }
-        $skip_row = ($current_page - 1) * $per_page;
-        //$projectName = ',(SELECT p.name FROM projects as p WHERE p.id = r.project_id) as project';
-       // $query = DB::table('requirements as r')->whereRaw($str_srch)->selectRaw('r.id,r.description,r.status_id'.$projectName);
-        $query = DB::table('os_suppliers as s')
-                ->join('os_affiliates as sa','sa.id','=','s.referrer_id')
-                ->whereRaw($str_srch)
-                ->whereRaw($str_where)
-                ->selectRaw('s.id ,s.code, s.name, s.phone_number,s.photo_file_name, s.email, s.address, s.status_code,s.branch_id, s.price_list_id,getPriceListName(s.price_list_id) AS price_list_name,s.referrer_id,sa.type_from_affilliate_type,sa.name as sales_agent,s.create_user,formatDate(s.create_date) as created_at,DATE_FORMAT(s.create_date,\'%r\') AS request_time' )->orderBy('s.id', 'DESC');;
-       
-        // return $query;
-        $clone_query = clone $query;
-        $count = $clone_query->count('s.id');
-        // $login_accounts = DB::table('um_users')->selectRaw('official_id')->get();
-        $rows = $query->skip($skip_row)->take($per_page)->get();
-        foreach($rows as $row){
-            $row->image_url = '';
-            if($row->photo_file_name) $row->image_url = PublicStorage::getUrl($row->branch_id,'general','image').$row->photo_file_name;
-            unset($row->photo_file_name);
-            if(!$row->image_url) $row->image_url =self::defaultImage($ss->branch_id);
-        }
-        return new LengthAwarePaginator($rows,$count,$per_page,$current_page);
-        // return $rows;
-
-    }
-
-    static function defaultImage($branch_id){
-        return PublicStorage::getUrl($branch_id,'default','image').'mr3.jpg';
-    }
-
-    function setPriceList($price_list_id,$id=null,$ss =null){
-        $ss = $ss ?? $this->userInfo;
-        $id = $id ?? $this->id;
-        $branch_id = Sanitizer::sanitize($ss->branch_id);
-        $p = getDataRow('os_supplier_price_list_names',["id"=>$price_list_id],"id,name");
-        if(!$p) return DV::error("Price list ID is not valid");
-        $p_name = $p->name;
-        DB::table('os_suppliers')->where('id',$id)->update(array(
-        'price_list_id'=>$price_list_id));
-        // return JDV::result($price_list_id );
-        return DV::success(['list_name'=>$p_name,'list_id'=>$price_list_id]);
-    }
-
-    // function details($id,$ss){
-    //     $id = $id ?? $this->id;
-    //     $branch_id = $ss->branch_id;
-
-    //     $row = DB::table('requirements as r')->where('r.id',$id)->where('r.branch_id',$branch_id)->selectRaw('r.id,r.project_id,r.description,r.status_id')->first();
-    //     return $row;
-    // }
     
-    function delete($id){
-
-        $id = $id ?? $this->id;
-
-        $delete = DB::table('os_suppliers')->where('id',$id)->delete();
-        return DV::depends($delete,['action','deleted']);
-    }
-
-    function getNextSenderCode($uss,$len =4){
-        $branch_id = $uss->branch_id;
-        $prefix ='SP';
-        $str_prefix = $prefix? 'prefix =\''.$prefix.'\'' : '2=2';
-        $row = DB::table('sender_code_control AS c')->where('branch_id',$branch_id)->whereRaw($str_prefix)->selectRaw('TRIM(c.prefix) AS prefix,c.last_id')->take(1)->first();
-        if($row) {
-            $num = $row->last_id;
-            $prefix = trim($row->prefix);
-            $num +=1;
-            DB::table('sender_code_control')->where('branch_id',$branch_id)->whereRaw($str_prefix)->update(['last_id'=>$num]);
-            return $prefix.$branch_id.formatNumber($num,$len);
-        }
-        DB::table('sender_code_control')->insert(array('branch_id'=>$branch_id,'last_id'=>1,'prefix'=>$prefix));
-        return $prefix.$branch_id.formatNumber(1,$len);
-    }
-    
-    function updateStatus($status_code,$id=null,$ss=null){
-        $ss = $ss?$ss:$this->userInfo;
-        $id = $id?$id:$this->id;
-        if(in_array(strtolower($status_code),['inactive','locked','disabled'])){
-            $err = self::getOutstandingBalanceError($id);
-            if($err) return DV::error($err);
-            }
-        $x = DB::table('os_suppliers')->where('id',$id)->update([
-            'status_code'=>$status_code
-        ]);
-        return DV::depends($x,['Supplier status','updated']);
-        //if(!$x) return DV::error('It seems that provided merchant identity does not exist');
-        // $um = new \App\Models\UM();
-        // $user_id = DB::table('um_users')->where('official_id',$id)->take(1)->value('id');
-        // $res = $um->setUserStatus($status_code,$user_id);
-        // return $res;
-    }
-    static function getOutstandingBalanceError($id){
-     $row = DB::table('package as p')->join('sender as s','s.id','=','p.sender_id')->where('p.status_id',8)->where('s.id',$id)->whereRaw('IFNULL(p.sender_pmt_status_id,0) =0')->selectRaw('COUNT(p.id) AS item_count,SUM(IFNULL(p.sender_total,0)) AS amount')->get()->first();
-     if(!$row) return null;
-     if ($row->item_count > 0 ) return 'មិន​អាច​លុប ឬ​បិទ​គណនី​នេះ​បាន​ទេ ព្រោះ​មាន​កញ្ចប់ '.$row->item_count.' ដែល​មិន​ទាន់​បាន​ទូទាត់​ប្រាក់';
-     return null;
-    }
-    static function getSliceCircle($status_id){
-        $colors = [1=>'#E3EA00',2=>'#6200EA',3=>'#33EA00'];
-        return $colors[$status_id];
-    }
 
     static function getHeaderCards($ss){   
         $branch_id = $ss->branch_id;
@@ -277,16 +133,52 @@ class Dashboard //extends Model
         $end_date = new DateTime();
         $end_date = $end_date->format('Y-m-d'); 
         $str_dates = "DATE(i.create_date) >= '$start_date' AND DATE(i.create_date) <= '$end_date'";
-        $total_shipment = DB::table('os_shipments as i')->where('i.status_id',3)->whereRaw($str_dates)->count('i.id');
+        // $total_shipment = DB::table('os_shipments as i')->where('i.status_id',3)->whereRaw($str_dates)->count('i.id');
+        $status_ids = [1,2];
+        // return $rows = DB::table('os_customer_invoices as i')->whereIn('i.status_id',$status_ids)->whereRaw($str_dates)->selectRaw('COUNT(i.id) as cnt, i.status_id, i.amount_due')->groupByRaw('i.status_id')->get();
+
         $cnt_invoice = DB::table('os_customer_invoices as i')->whereRaw($str_dates)->count('i.id');
-        $cnt_invoice_paid =  DB::table('os_customer_invoices as i')->where('i.status_id',2)->count('i.id');
-        $total = 0; 
+        $cnt_invoice_paid =  DB::table('os_customer_invoices as i')->where('i.status_id',2)->whereRaw($str_dates)->count('i.id');
+        $invoce_amount =  DB::table('os_customer_invoices as i')->whereRaw($str_dates)->selectRaw('i.amount_due')->get();
+        $invoce_amount_paid =  DB::table('os_customer_invoices as i')->where('i.status_id',2)->whereRaw($str_dates)->selectRaw('i.amount_due')->get();
+        
+        $total_invoce_amount = 0;
+        foreach($invoce_amount as $amount){
+            $total_invoce_amount += $amount->amount_due; 
+        }
+        $total_invoce_amount_paid =0;
+        foreach($invoce_amount_paid as $amount_paid){
+            $total_invoce_amount_paid += $amount_paid->amount_due; 
+        }
+
+        
+
+        $cnt_bill_payment = DB::table('os_payments as i')->whereRaw($str_dates)->count('i.id');
+        $cnt_bill_payment_paid =  DB::table('os_payments as i')->join('os_bill_payments as b','b.payment_id','=','i.id')->whereRaw($str_dates)->count('i.id');
+        $bill_payment_amount =  DB::table('os_payments as i')->whereRaw($str_dates)->selectRaw('i.amount')->get();
+        $bill_payment_amount_paid =  DB::table('os_payments as i')->join('os_bill_payments as b','b.payment_id','=','i.id')->whereRaw($str_dates)->selectRaw('b.total_price')->get();
+        
+        $total_bill_payment_amount = 0;
+        foreach($bill_payment_amount as $payment){
+            $total_bill_payment_amount += $payment->amount; 
+        }
+        $total_bill_payment_amount_paid = 0;
+        foreach($bill_payment_amount_paid as $payment_paid){
+            $total_bill_payment_amount_paid += $payment_paid->total_price; 
+        }
 
         $progress_cards [] = (object)[
-            'total_invoive'=> $cnt_invoice,
-            'invoce_percent'=> number_format($cnt_invoice *100 / $total_shipment,2),
-            'total_shipment'=> $total_shipment,
-            'color'=> '',
+            'cnt_invoice'=> $cnt_invoice,
+            'invoce_paid_percent'=> number_format($cnt_invoice_paid *100 / $cnt_invoice,2),
+            'total_invoce_amount'=> number_format($total_invoce_amount,2),
+            'invoce_amount_paid '=> $total_invoce_amount_paid,
+            'invoice_color'=> '',
+
+            'cnt_bill_payment'=> $cnt_bill_payment,
+            'bill_payment_paid_percent'=> number_format($cnt_bill_payment_paid *100 / $cnt_bill_payment,2),
+            'total_bill_payment_amount'=> $total_bill_payment_amount,
+            'total_bill_payment_amount_paid '=> $total_bill_payment_amount_paid,
+            'invoice_color'=> '',
         ];
         return (object)[
             'progress_cards' => $progress_cards,
@@ -373,38 +265,4 @@ class Dashboard //extends Model
         // return $data;
     }
 
-     function saveProfilePicture($photo_data,$file_type = null,$id=null,$ss=null){
-        $id = $id?$id:$this->id;
-        $ss = $ss?$ss:$this->userInfo;
-        $supplier = DB::table('os_suppliers')->where('id',$id)->selectRaw('id,branch_id,photo_file_name')->first();
-        $delete_image = (!$photo_data || isImage($photo_data));
-        if(!$supplier)return DV::error('Supplier identity is not correct!');
-        if($delete_image){
-          PublicStorage::delete($ss->branch_id,'general','image',$supplier->photo_file_name);
-          DB::table('os_suppliers')->where('id',$id)->update(['photo_file_name'=>null]);
-        }
-        return PublicStorage::saveImage($ss->branch_id,self::$img_dir, null,$photo_data,null,['id'=>$id,'store'=>'os_suppliers.photo_file_name']);  
-        
-      }
-      static function getProfilePicture($id)
-  {
-    $row = DB::table('os_suppliers ')->where('id', $id)->selectRaw('branch_id,photo_file_name')->first();
-    if (!$row) {
-      return self::defaultImage(1);
-    }
-    $url = PublicStorage::getUrl($row->branch_id, 'Supplier', 'image') . $row->photo_file_name;
-    return validateUrl($url, '');
-  }
-
-      function deleteProfilePicture($id = null, $ss = null)
-      {
-        $id = $id ?? $this->id;
-        $ss = $ss ?? $this->userInfo;
-        $supplier = DB::table('os_suppliers')->where('id', $id)->selectRaw('id,branch_id,photo_file_name')->first();
-        if (!$supplier)
-          return DV::error('Supplier identity is not correct!');
-        PublicStorage::delete($ss->branch_id, 'Supplier', 'image', $supplier->photo_file_name);
-        DB::table('os_suppliers')->where('id', $id)->update(['photo_file_name' => null]);
-        return DV::depends($id,['Supplier are','update']);
-      }
 }
