26e7a008cfa96173fc9451495cb98ac3a4287f04
Krymengchhorng <mengchhorngk@gmail.com>
2024-10-10 09:23:16 +0700

    update

--- public/js/components/bhr/EmployeeComponent.js
+++ public/js/components/bhr/EmployeeComponent.js
@@ -12,8 +12,9 @@ var EmployeeComponent = new (function () {
     this.divFilter = this.self.querySelector("#_divFilter_emp");
     this.elSearch = this.self.querySelector("#_sdl_search_employee");
     this.containerPagination = mThis.self.querySelector('#container_pagination');
-
-
+    this.profile_card_detail = mThis.self.querySelector('#profile_card_detail');
+    this.profile_info_emp = mThis.self.querySelector('#profile_info_emp');
+   
 
     let div = mThis.self.querySelector('#_employee_list');
     this.init= () => {
@@ -197,13 +198,215 @@ var EmployeeComponent = new (function () {
     };
 
 
-
-
-
-
-
-
-
+    this.renderProfile = (data) => {
+        let html = `
+                    <div class="d-block ms-3 w-100">
+                        <div class="row row-cols-3 mb-0">
+                            <div class="col-2">
+                                <div class="div-img">
+                                    <img src="${data.image_url || '../uploads/public/1_data/default/images/mr.avif'}" alt="Employee Image">
+                                </div>
+                                <div class="social-icons d-flex justify-content-start mt-3">
+                                    <a href="#" class="mx-2"><img src="assets/images/bhr/facebook.svg" alt="Facebook"></a>
+                                    <a href="#" class="mx-2"><img src="assets/images/bhr/linkedin.svg" alt="Linkedin"></a>
+                                    <a href="#" class="mx-2"><img src="assets/images/bhr/telegram.svg" alt="Telegram"></a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Name</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap text-capitalize">${data.name}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Position</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap">${data.position || ''}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Email</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-primary">${data.email || ''}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Tel</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap">${data.phone_number || ''}</p>
+                                </div>
+                                  <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">ID</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap">${data.code || ''}</p>
+                                </div>
+                                
+                            </div>
+                            <div class="col">
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Nationality</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap">${data.nationality || ''}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted width-p">Date of Birth</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap">${data.date_of_birth || ''}</p>
+                                </div>
+                                <div class="d-flex align-items-center">
+                                    <p class="text-nowrap text-muted   width-bp" vslang="titles.Address">Address</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap text-capitalize">${data.address}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted   width-p" vslang="titles.NSSF">NSSF</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap text-capitalize">${data.nssf_id}</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="text-nowrap text-muted   width-p" vslang="titles.Identity Card">Identity Card</p>
+                                    <p class="px-2">:</p>
+                                    <p class="text-nowrap text-capitalize">${data.nid}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `;
+    
+        this.profile_info_emp.innerHTML = html;
+    };
+    this.renderCardDetail = () => {
+        let html = '';
+        html = [
+            `
+            <div class="col-md-4">
+                <div class="card" style="height:487px;">
+                    <div class="card-header">
+                        <h4>Skills</h4>
+                        <span class="ellipsis">...</span>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <p>PHP</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 85%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>JavaScript</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 70%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>Node.Js</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 50%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>Vue.Js</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 65%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>Laravel</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 60%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>OOP</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 80%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>Next.Js</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 40%;"></div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>React.Js</p>
+                            <div class="progress" style="width: 60%;">
+                                <div class="progress-bar" style="width: 60%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="height:487px;">
+                    <div class="card-header">
+                        <h4>Education</h4>
+                        <span class="ellipsis">...</span>
+                    </div>
+                    <div class="card-body">
+                        <div class="">
+                            <h5>2022-2024</h5>
+                            <div class="d-flex justify-content-between">
+                                <p class="w-50">Associate Degree</p>
+                                <p class="text-primary w-75">University of Oxford</p>
+                            </div>
+                            <span class="text-muted">Web Development</span>
+                        </div>
+                        <div class="mt-3">
+                            <h5>2022-2024</h5>
+                            <div class="d-flex justify-content-between">
+                                <p class="w-50">Associate Degree</p>
+                                <p class="text-primary w-75">University of Cambridge</p>
+                            </div>
+                            <span class="text-muted">Web Development</span>
+                        </div>
+                        <div class="mt-3">
+                            <h5>2022-2024</h5>
+                            <div class="d-flex justify-content-between">
+                                <p class="w-50">Associate Degree</p>
+                                <p class="text-primary w-75">Royal University of Phnom penh</p>
+                            </div>
+                            <span class="text-muted">Web Development</span>
+                        </div>
+                        <div class="mt-3">
+                            <h5>2022-2024</h5>
+                            <div class="d-flex justify-content-between">
+                                <p class="w-50">Associate Degree</p>
+                                <p class="text-primary w-75">Massachusetts Institute of Technology</p>
+                            </div>
+                            <span class="text-muted">Web Development</span>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="height:487px;">
+                    <div class="card-header">
+                        <h4>Experience</h4>
+                        <span class="ellipsis">...</span>
+                    </div>
+                    <div class="card-body">
+                     <div class="">
+                        <h6>02-02-2023 - 14-11-2024</h6>
+                        <h5>Web Developer</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Expedita.</p>
+                        <p class="experience-company">Vectorasoft Company</p>
+                        <hr class="border border-warning">
+                     </div>
+                     <div class="">
+                        <h6>02-02-2023 - 14-11-2024</h6>
+                        <h5>Web Developer</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Expedita.</p>
+                        <p class="experience-company">Vectorasoft Company</p>
+                     </div>
+                 
+                    </div>
+                </div>
+            </div>`
+        ].join('');
+        this.profile_card_detail.innerHTML = html;
+    }
+    
+    
     mThis.elSearch.addEventListener('keyup', (e) => {
         clearTimeout(mThis.search_timeout);
         mThis.search_timeout = setTimeout(() => {
