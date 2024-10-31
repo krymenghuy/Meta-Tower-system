b2360164b4dfbc0eb6e6aab70de57e09a440c189
ratanak-khoeurn <ratanak.khoeurn@student.passerellesnumeriques.org>
2024-10-31 13:11:53 +0700

    update

--- resources/views/layouts/bhr/leaveComponent.blade.php
+++ resources/views/layouts/bhr/leaveComponent.blade.php
@@ -9,11 +9,11 @@
             </div>
             <div class="d-flex justify-content-end gap-3 w-50">
                 <div class="d-flex align-items-center w-50">
-                    <input data-select="datepicker" class="form-control filter-field" data-field="start_date"
+                    <input data-select="datepicker" class="form-control filter-field rounded-5" data-field="start_date"
                         placeholder="Start Date" id="_leave_filter_start_date" />
                 </div>
                 <div class="d-flex align-items-center w-50">
-                    <input data-select="datepicker" class="form-control filter-field" data-field="end_date"
+                    <input data-select="datepicker" class="form-control filter-field rounded-5" data-field="end_date"
                         placeholder="End Date" id="_leave_fliter_end_date" />
                 </div>
 
@@ -26,9 +26,12 @@
                     <select id="el_status" class="data-input filter-field " data-field="status"></select>
                 </div>
             </div>
-            <div class="d-flex justify-content-end  w-50">
-                <div class="d-flex align-items-center w-100">
-                    <input type="text" class="form-control filter-field rounded-3" id="_sdl_search_leave"
+            <div class="d-flex justify-content-end w-50 gap-3">
+                <div class="d-flex align-items-center w-50">
+                    
+                </div>
+                <div class="d-flex align-items-center w-50">
+                    <input type="text" class="form-control filter-field rounded-5" id="_sdl_search_leave"
                         placeholder="Search here....">
                 </div>
             </div>
