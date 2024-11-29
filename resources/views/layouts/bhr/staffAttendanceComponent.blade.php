d0ae51daa920ab01f71c7c7a01073485c036a345
Krymengchhorng <mengchhorngk@gmail.com>
2024-11-29 12:12:53 +0700

    get

--- resources/views/layouts/bhr/staffAttendanceComponent.blade.php
+++ resources/views/layouts/bhr/staffAttendanceComponent.blade.php
@@ -1,24 +1,26 @@
 <div id="_main_staffAttendanceComponent" style="display:none;padding:20px 0 0">
-    <div class="d-flex  p-4 justify-content-between w-200 " id="_divFilter_attendance">
-        <div class="d-flex align-items-center w-100 gap-2">
-            <div class="d-flex align-items-center w-50 gap-2">
-                <input type="text" class="form-control filter-field btn_search" id="_staff_attendance_search"
-                    placeholder="Search attendance ...">
-
-                <button id="_sdl_btnSearch" role="button" class="btn btn-primary rounded-5">
-                    <i class="la la-search"></i>
-                </button>
-            </div>
+    <div class="d-flex p-3 justify-content-between w-100 " id="_divFilter_attendance">
+        <div class="d-flex align-items-center w-50 gap-2">
+                <div class="form-group">
+                    <label for="academic_year" class="form-label " vslang="titles.Branch"></label>
+                    <div class="d-flex align-items-start w-50">
+                        <select id="el_emp_branch" class="data-input filter-field" data-field="branch_id"></select>
+                    </div>
+                </div>
+            
         </div>
         <div class="d-flex align-items-center justify-content-end gap-2 w-100">
-                <div class="d-flex align-items-center w-50">
-                    <input data-select="datepicker" class="form-control filter-field form_input" data-field="attendance_date"
-                        placeholder="Attendance Date" id="_filter_attendance_date" />
-                </div>
+                
             <button type="button" class="btn_add" id="_btnAddStaffAttendance">
                 <i class="fas fa-plus"></i>
                 <span>Add StaffAttendance </span>
             </button>
+            <div class="d-flex align-items-center w-50 gap-2">
+                <input type="text" class="form-control filter-field btn_search" id="_staff_attendance_search"
+                    placeholder="Search attendance ...">
+
+             
+            </div>
         </div>
     </div>
     <div id="_staff_attendance_list" class="m-4"></div>
