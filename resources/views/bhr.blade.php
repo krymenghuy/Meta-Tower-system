a73e59d1c39581530fbae596f7c41cba8da6e3db
samsethy@gmail.com <samsethy@gmail.com>
2025-01-19 13:45:49 +0700

    Update1

--- resources/views/bhr.blade.php
+++ resources/views/bhr.blade.php
@@ -73,8 +73,8 @@
     <?php
     ScriptManager::render('priority-one', 1, 3);
     ScriptManager::render('primary', 1, 6);
-    ScriptManager::render('bhr-primary-defer', 1, 10);
-    ScriptManager::render('bhr-components', 1, 130);
+    ScriptManager::render('bhr-primary-defer', 1, 11);
+    ScriptManager::render('bhr-components', 1, 131);
 
     //ScriptManager::render('pdfmake', 1);
     ?>
