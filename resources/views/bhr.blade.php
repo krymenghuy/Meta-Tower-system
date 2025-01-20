108026382e9955eddca3c1b1677911c7e7003a95
YDarit <yangdarit@gmail.com>
2025-01-20 16:58:11 +0700

    update:

--- resources/views/bhr.blade.php
+++ resources/views/bhr.blade.php
@@ -74,7 +74,7 @@
     ScriptManager::render('priority-one', 1, 3);
     ScriptManager::render('primary', 1, 6);
     ScriptManager::render('bhr-primary-defer', 1, 11);
-    ScriptManager::render('bhr-components', 1, 133);
+    ScriptManager::render('bhr-components', 1, 134);
 
     //ScriptManager::render('pdfmake', 1);
     ?>
