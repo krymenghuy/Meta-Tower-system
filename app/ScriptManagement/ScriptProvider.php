90cb577f1ab2ba3162f0cf34b4c84e8be53599a3
ratanak-khoeurn <ratanak.khoeurn@student.passerellesnumeriques.org>
2025-02-06 15:50:47 +0700

    fix

--- app/ScriptManagement/ScriptProvider.php
+++ app/ScriptManagement/ScriptProvider.php
@@ -139,6 +139,7 @@ class ScriptProvider
                 'js/components/umt/FindUserDialog.js',
                 'js/components/umt/BranchManagementComponent.js',
                 'js/components/umt/RoleManagementComponent.js',
+                // 'js/components/umt/RoleManagementTool.js',
                 'js/components/bhr/CreateContractDialog.js',
             ]
         ],
