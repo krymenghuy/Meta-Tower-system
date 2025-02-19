67108dbb588a8af5017b32ede5e3f8feb8bcce64
samsethy@gmail.com <samsethy@gmail.com>
2025-02-19 08:52:23 +0700

    update

--- app/Http/Controllers/ReportCenterController.php
+++ app/Http/Controllers/ReportCenterController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use XAuthService;
 use JDV;
-use App\Models\Umt\User;
+use XUser;
+use XAuthservice;
 use DB;
   
 class ReportCenterController extends Controller
@@ -13,18 +13,28 @@ class ReportCenterController extends Controller
     function getReportList(Request $req){
         $ss = XAuthService::verifyAuth($req,-1);
         if($ss->status_code !=200) return JDV::raw($ss);
-        $include = '';
         $user_id = $ss->user_id;
-        if($ss->is_master_account !== 1){
-            $umm_prms = DB::table('um_user_permissions as up')->join('um_permissions as p','p.id','=','up.permission_id',)->where('user_id', $user_id)->where('p.category','report')->pluck('p.name')->toArray();
-            if(count($umm_prms)> 0){
-                $include = ' AND name IN (\'' . implode('\',\'', $umm_prms) . '\')';
-            }else{
-                $include = ' AND 1 = 0';
-            }
+        $app_id = $req->app_id ?? null;
+        
+        $access_reports = XUser::getReports ($user_id,$app_id,null);
+        $rows = DB::table('reports as rpt')->where('app_id',hex2bin($app_id))->selectRaw('rpt.id,rpt.permission_id,rpt.name,rpt.code,rpt.category,rpt.params,rpt.module_id,rpt.display_order,rpt.hidden')->orderByRaw('display_order ASC')->get();
+        $reports = [];
+        $is_master_account = $ss->is_master_account ?? 0;
+        foreach($rows as $row){
+            if (!$is_master_account){
+                $permission_id = $row->permission_id ?? null;
+                if ($permission_id > 0){
+                    $founds = $access_reports ->filter(function($can_access_report) use($permission_id){
+                        return $can_access_report->permission_id == $permission_id;
+                    });
+                    if(!$founds->isEmpty()){
+                        $reports[] = $row;
+                    }
+                }else $reports[] = $row;
+               
+            } else $reports[] = $row;
         }
-      $rows = DB::select("SELECT id, `name`, `hidden`, code,category,rpt.module_id,rpt.params,rpt.display_order,rpt.hidden FROM reports AS rpt WHERE IFNULL(rpt.hidden,0) = 0 $include ORDER BY rpt.display_order ASC");
-      return JDV::json($rows);
+      return JDV::json($reports);
    }
 
    static function appExists($bin_app_id){
@@ -38,12 +48,11 @@ class ReportCenterController extends Controller
     $bin_app_id = $app_id ? hex2bin($app_id):null;
     if(!$bin_app_id) return [];
     if(!self::appExists($bin_app_id)) return [];
-
-   
+    
     $user_id = $ss->user_id;
     $is_master_account = $ss->is_master_account ?? 0;
 
-    $access_reports = User::getReports ($user_id,$app_id,null);
+    $access_reports = XUser::getReports ($user_id,$app_id,null);
     $reports = DB::table('reports as rpt')->where('rpt.app_id',$bin_app_id)->whereRaw('IFNULL(rpt.hidden,0) =0')->selectRaw('id, permission_id,`name`, `hidden`, code,category,rpt.module_id,rpt.params,rpt.export_pdf, export_excel,export_csv, rpt.category_order,rpt.display_order,rpt.hidden')->get();
     $allowed_reports = [];
     foreach($reports as $row){
