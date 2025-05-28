c9dc6e9133aa0eb4291889e8215f44d052ec3fc2
Krymengchhorng <mengchhorngk@gmail.com>
2025-05-28 09:44:37 +0700

    get

--- public/js/components/ypg/DashboardComponent.js
+++ public/js/components/ypg/DashboardComponent.js
@@ -1,6 +1,4 @@
 "use strict";
-
-
 var DashboardComponent =  (function () {
     const mThis = {};
     mThis.title_prop = "Dashboard";
--- resources/views/layouts/ypg/dashboardComponent.blade.php
+++ resources/views/layouts/ypg/dashboardComponent.blade.php
@@ -11,7 +11,6 @@
 
     </div>
 </div>
-
 <style>
     pre {
         margin: 0;
