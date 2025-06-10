11410ca2aaae73e9be8900103493883172357088
Krymengchhorng <mengchhorngk@gmail.com>
2025-06-10 16:55:51 +0700

    get

--- resources/views/layouts/ypg/policyComponent.blade.php
+++ resources/views/layouts/ypg/policyComponent.blade.php
@@ -1,8 +1,17 @@
-<div id="_main_policy_component" class="mt-4" style="display: none; ">
-      <div id="_policy_list" class="pt-3 p-4"> 
-
-      </div>
+<div id="_main_policy_component" class="mt-4" style="display: none;">
+    <div id="_policy_list" class="pt-3 p-4"> 
+    </div>
 </div>
+<style>
+   .policy-section h2 {
+  font-weight: bold;
+  margin-top: 1rem;
+  color: #333;
+}
 
-
-    
+.policy-section p {
+  margin-bottom: 0.75rem;
+  line-height: 1.6;
+  color: #444;
+}
+</style>
