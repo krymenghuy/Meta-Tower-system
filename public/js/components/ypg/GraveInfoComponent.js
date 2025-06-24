a2c7fc82ce23900fcb54368b3f63a3b00547f107
sivminh <sivminhkuoch@gmail.com>
2025-06-24 11:20:58 +0700

    get

--- public/js/components/ypg/GraveInfoComponent.js
+++ public/js/components/ypg/GraveInfoComponent.js
@@ -265,14 +265,12 @@ vsapi.call(`${main_view.base_url}/ypg/grave-slot/delete`, op,{ }).then(res => {
     }
 
     mThis.prepareFormOptions = (onFinish = null) => {
-        vsapi.call(`${main_view.base_url}/ypg/grave-slot/form-options`, null, null, null)
-            .then(res => {
-                const d = res.status_code == 200 ? res.data : {};
-                console.log(12,res.data);
-                
-                VSUtil.setComboItems(mThis.elFilter_status, d.statuses, 'id', 'grave_status', true, 'All Statuses', null);
-               if(typeof onFinish ==='function') onFinish();
-            })
+        vsapi.call(`${main_view.base_url}/ypg/grave-slot/delete`, op,{ agent:null, loader:null, useCache:false }).then(res => {
+                    if (res.status_code == 200) {
+                        mThis.GraveInfoListView.showPage();
+                    }
+                });
+
     }
 
     mThis.show = function () {
