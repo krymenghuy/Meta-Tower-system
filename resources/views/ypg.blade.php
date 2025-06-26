9096fad1f72c8ce57fe7ebfe3c5e6e48be0efef0
Krymengchhorng <mengchhorngk@gmail.com>
2025-06-26 11:06:38 +0700

    get

--- resources/views/ypg.blade.php
+++ resources/views/ypg.blade.php
@@ -28,7 +28,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <?php
-    StyleManager::render('ypg-style', 1, 14);
+    StyleManager::render('ypg-style', 1, 15);
     ?>
     <style>
         /* Custom animations for the modal like Materialize css effect */
