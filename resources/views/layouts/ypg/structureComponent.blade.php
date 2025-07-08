4ee8c36047f9cce676a1b8fbb4915853a8cd9b36
Krymengchhorng <mengchhorngk@gmail.com>
2025-07-08 14:38:11 +0700

    get

--- resources/views/layouts/ypg/structureComponent.blade.php
+++ resources/views/layouts/ypg/structureComponent.blade.php
@@ -1,14 +1,26 @@
 <style>
-.structure-container {
+/* .structure-container {
   max-width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+} */
+ .structure-container {
+  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(rgb(39 68 74), rgb(228 228 228)),
+              url('{{ asset('assets/images/yavpheng/bg_structure.jpg') }}');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
+
 
 .structure-frame {
   position: relative;
-  width: 550px;
+  width: 460px;
   border: 1px solid #ddd;
   background-color:#27444a;
 }
@@ -22,17 +34,17 @@
 
 .img-structure {
   max-width: 100%;
-  height: 700px;
+  height: 630px;
 }
 
 </style>
 
-<div id="_main_structure_component">
-  <div class="structure-container">
-    <div class="structure-frame">
+<div id="_main_structure_component" style="display: none;">
+  <div class="structure-container" id="structureContainer">
+    <!-- <div class="structure-frame">
       <div class="structure-front-image">
         <img class="img-structure" src="{{ asset('assets/images/yavpheng/Structure_ypg.jpg') }}"/>
       </div>
-    </div>
+    </div> -->
   </div>
 </div>
