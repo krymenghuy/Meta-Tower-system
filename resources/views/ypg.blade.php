c1e168d243a297963fc8b08abf5a9cbda6aeaae4
samsethy@gmail.com <samsethy@gmail.com>
2025-07-10 23:30:23 +0700

    Update

--- resources/views/ypg.blade.php
+++ resources/views/ypg.blade.php
@@ -28,7 +28,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <?php
-    StyleManager::render('ypg-style', 1, 17);
+    StyleManager::render('ypg-style', 1, 18);
     ?>
     <style>
         /* Custom animations for the modal like Materialize css effect */
@@ -88,9 +88,9 @@
     </style>
     <?php
     ScriptManager::render('priority-one', 1, 9);
-    ScriptManager::render('primary', 1, 12);
-    ScriptManager::render('primary-defer', 1, 29);
-    ScriptManager::render('ypg-components', 1, 216);
+    ScriptManager::render('primary', 1, 13);
+    ScriptManager::render('primary-defer', 1, 30);
+    ScriptManager::render('ypg-components', 1, 217);
     //ScriptManager::render('pdfmake', 1);
     ?>
 </head>
@@ -139,8 +139,7 @@
                                 <div class="show--title mx-4 p-3 flex-grow-1 ">
                                     <div style="height:18px" class="screen-info d-flex flex-row justify-content-between align-items-center  w-100">
                                         <div>
-                                            <h6 class="screen-title mb-0 " vslang="titles.dashboard"
-                                          style="color:#ffffff;" id="screen_title">Dashboard</h6>
+                                            <span class="screen-title mb-0 " vslang="titles.dashboard" id="screen_title"></span>
                                        </div>
                                     </div>
                                 </div>
