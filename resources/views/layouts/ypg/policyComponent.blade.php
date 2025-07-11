cc2686a42a74e3e9ec0dee0168d2eccb868eff8f
Krymengchhorng <mengchhorngk@gmail.com>
2025-07-11 15:55:39 +0700

    get

--- resources/views/layouts/ypg/policyComponent.blade.php
+++ resources/views/layouts/ypg/policyComponent.blade.php
@@ -1,10 +1,37 @@
-<div id="_main_policy_component" class="mt-4" style="display: none; align-items: center;">
-      <div id="imageStructureBox" class="mt-3">
-        <!-- <div class="d-flex justify-content-center">
-            <img src="{{ asset('assets/images/yavpheng/Structure_ypg.jpg') }}" style="width:35%; height: 35%; ">
-        </div> -->
-      </div>
-       
-        
+<div id="_main_policy_component" style="display: none;">
+  <div id="imageStructureBox"></div>
 </div>
+<style>
+  #_main_policy_component {
+  display: flex;
+  justify-content: center;
+  margin-top: 0.5rem;
+  background: linear-gradient(rgb(39, 68, 74), rgb(228, 228, 228));
+  padding: 25px 10px;
+}
 
+.policy-scroll-container {
+  overflow: auto;
+  max-height: 85vh;
+}
+
+.policy-img-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
+.policy-img {
+  width: 49%;
+  height: auto;
+  border-radius: 6px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+}
+
+.policy-img-left {
+  margin-right: 0;
+}
+
+</style>
