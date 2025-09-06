29c9c51d1cedd0b980fa462a7609c115e3f02b97
Krymengchhorng <mengchhorngk@gmail.com>
2025-09-06 11:18:44 +0700

    get

--- resources/views/menus/prm_menu.blade.php
+++ resources/views/menus/prm_menu.blade.php
@@ -177,7 +177,7 @@ function v_display($mod_id, $module_ids = null)
                         </li> -->
 
                         <li class="kt-menu__item" aria-haspopup="true" <?php v_display(239); ?>>
-                            <a href="MemberComponent" modid="239" class="menu-item kt-menu__link">
+                            <a href="TenantComponent" modid="239" class="menu-item kt-menu__link">
                                 <span class="kt-menu__link-icon">
                                     <img class="icons opacity-icons" src="{{ asset('assets/images/icons/member.png') }}" /></span>
                                 <span class="kt-menu__link-text font-color " vslang="menus.Tenant"></span>
