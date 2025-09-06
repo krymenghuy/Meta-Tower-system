23d1aead1ccff5fa21509b2cd0d64cfb484f04db
sivminh <sivminhkuoch@gmail.com>
2025-09-06 10:14:06 +0700

    upd

--- resources/views/layouts/prm/graveInfoComponent.blade.php
+++ resources/views/layouts/prm/graveInfoComponent.blade.php
@@ -1,5 +1,5 @@
 <div id="_main_grave_info_component" class="m-3 pt-2" style="display:none;">
-    <div class="d-flex justify-content-between w-100 rounded-2 shadow p-3 mt-2" style="background-color:#27444a;" id="_divFilter_grave_info" >
+    <div class="d-flex justify-content-between w-100 rounded-2 shadow p-3 mt-2" style="background-color:#1A1647;" id="_divFilter_grave_info" >
         <div class="d-flex justify-content-start gap-3 w-50">
             <div class="d-flex w-50">
                 <input type="text" class="form-control rounded-2 filter-field" id="_search_grave_info" placeholder="Search">
--- resources/views/layouts/prm/memberComponent.blade.php
+++ resources/views/layouts/prm/memberComponent.blade.php
@@ -1,5 +1,5 @@
 <div id="_main_member_component" class="mobile-padding p-3" style="display:none;">
-    <div class="d-flex justify-content-between w-100 rounded-2 shadow mt-3 p-3" style="background-color:#27444a;" id="_divFilter_member">
+    <div class="d-flex justify-content-between w-100 rounded-2 shadow mt-3 p-3" style="background-color:#1A1647;" id="_divFilter_member">
             <div class="d-flex justify-content-start gap-3 w-50">
                 <div class="d-flex w-50">
                     <input type="text" class="form-control rounded-2 filter-field" id="_search_member" placeholder="Search Tenant   ">
