83e1998946f803fb01760f69b29163ced8561a61
Krymengchhorng <mengchhorngk@gmail.com>
2025-09-11 10:08:33 +0700

    get

--- app/Http/Controllers/Prm/TenantController.php
+++ app/Http/Controllers/Prm/TenantController.php
@@ -10,5 +10,19 @@ use Illuminate\Http\Request;
 
 class TenantController extends Controller
 {
-    //
+    protected $tenants;
+    public function __construct(){
+        $this->tenants = new Tenant();
+    }
+
+    public function createTenant(Request $req){
+        $ss = XAuthService::verifyAuth($req, -1);
+        if($ss->status_code !==200){
+            return JDV::raw($ss);
+        }
+        $id = $req->id ?? $req->tenant_id;
+        $tenant = new Tenant($id,$ss);
+        $create_tenant = $tenant->createTenant($req->all());
+        return JDV::raw($create_tenant);
+    }
 }
--- app/Models/Prm/Tenant.php
+++ app/Models/Prm/Tenant.php
@@ -17,4 +17,26 @@ class Tenant
         $this->id = $id;
         $this->userInfo = $userInfo;
     }
+
+    public function createTenant($arr = [],$id = null, $ss = null){
+        $id = $id ?? $this->id;
+        $ss = $ss ?? $this->userInfo;
+
+        $v_rule = [
+            'name' => '1|string|0-200|text=Tenant name must be provided',
+            'legal_name' => '0|string|0-250',
+            'phone_number' => '0|phone|0-23',
+            'email' => '0|email|1-50',
+            'address' => '0|string|0-350',
+        ];
+        $email_char = ['@','.','-','_'];
+        $address_char = ['@',',','.','#'];
+        $res = DBX::validateObject($arr,$v_rule,1,['email'=>$email_char,'address'=> $address_char],$ss->lang,0,null);
+        if($res->error) return DV::error($res->error);
+        $inputs = $res->values;
+        if(!$id) {
+            $exist
+        }
+
+    }
 }
