caa145c778ce36a47a6809f07b5eed44099ea7d9
sivminh <sivminhkuoch@gmail.com>
2026-02-19 08:24:10 +0700

    get

--- config/script_bundles.php
+++ config/script_bundles.php
@@ -127,7 +127,7 @@ return [
                 'js/components/prm/ReportComponent.js',
                 'js/components/prm/ExpenseComponent.js',
                 'js/components/prm/ServiceRequestComponent.js',
-                'js/components/prm/ChargeableComponent.js',
+                'js/components/prm/ReservationComponent.js',
                 
 
 
--- resources/views/prm.blade.php
+++ resources/views/prm.blade.php
@@ -324,7 +324,7 @@
                             @include('layouts.prm.reportComponent')
                             @include('layouts.prm.expenseComponent')
                             @include('layouts.prm.serviceRequestComponent')
-                            @include('layouts.prm.chargeableComponent')
+                            @include('layouts.prm.reservationComponent')
 
 
                         </div>
