3ddd3df21f217b5d8522757724c060f56f1b2dd9
sivminh <sivminhkuoch@gmail.com>
2026-03-11 11:06:42 +0700

    udp

--- app/Models/Prm/GeneralSettings.php
+++ app/Models/Prm/GeneralSettings.php
@@ -537,4 +537,9 @@ class GeneralSettings //extends Model
         return $rows;
     }
 
+    static function options_amenity_category($ss)
+    {
+        return DB::table('amenity_categories')->selectRaw('id,name as amenity_category')->get();
+    }
+
 }
