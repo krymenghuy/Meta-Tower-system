f5cc22a46a5595d99d9ee040690d6c5f2fbffb41
Sivminhh <sivminhkuoch@gmail.com>
2026-04-23 08:25:41 +0700

    get

--- resources/views/layouts/prm/billComponent.blade.php
+++ resources/views/layouts/prm/billComponent.blade.php
@@ -2,7 +2,7 @@
      <div id="_divFilter_bill" class="rounded-2 p-3 bg-white shadow-sm">
         <div class="row g-3 align-items-center">
             <div class="col-12 col-md-6 col-lg-3">
-                <input type="text" class="form-control rounded-2 pe-5 filter-field input-search" id="_search_bill" placeholder="Search ____" >
+                <input type="text" class="form-control rounded-2 pe-5 filter-field input-search" id="_search_bill" placeholder="Search by Name or Bill Number" >
             </div>
             <div class="col-12 col-md-6 col-lg-2 d-none ">
                 <select type="id" id="_bill_vendor_id" class="data-input filter-field form-control" data-field="vendor_id"></select>
