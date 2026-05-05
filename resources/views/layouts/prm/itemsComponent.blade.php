2b7f1a0ea4b40460bc2f671137b8261c66814315
Krymengchhorng <mengchhorngk@gmail.com>
2026-05-05 08:25:07 +0700

    get

--- resources/views/layouts/prm/itemsComponent.blade.php
+++ resources/views/layouts/prm/itemsComponent.blade.php
@@ -4,7 +4,7 @@
             <div class="col-12 col-md-6 col-lg-3">
                 <input type="text" class="form-control rounded-2 pe-5 filter-field input-search" id="_search_item" placeholder="Search by name or code">
          </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-3">
                 <select type="id" id="_item_category_id" class="data-input filter-field form-control" data-field="category_id"></select>
             </div>
 
