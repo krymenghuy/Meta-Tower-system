5012a9b355761e9bada11fdff40e98adb88be68e
Krymengchhorng <mengchhorngk@gmail.com>
2026-06-02 11:22:42 +0700

    get

--- app/Http/Controllers/Prm/BuildingController.php
+++ app/Http/Controllers/Prm/BuildingController.php
@@ -55,7 +55,7 @@ class BuildingController extends Controller
         if ($ss->status_code !== 200) {
             return JDV::raw($ss);
         }
-        return JDV::result($this->buildings->getFormOptions($req->id, $req->building_id));
+        return JDV::result($this->buildings->getFormOptions($req->building_id, $req->id));
     }
     public function deleteBuilding(Request $req)
     {
