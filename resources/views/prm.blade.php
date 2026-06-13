6d483af20e97ff28b71098471cdbc78601105248
Krymengchhorng <mengchhorngk@gmail.com>
2026-06-13 09:16:55 +0700

    get

--- resources/views/prm.blade.php
+++ resources/views/prm.blade.php
@@ -28,7 +28,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <?php
-    StyleManager::render('prm-style', 1, 60);
+    StyleManager::render('prm-style', 1, 61);
     ?>
     <style>
         /* Custom animations for the modal like Materialize css effect */
@@ -99,7 +99,7 @@
     ScriptManager::render('priority-one', 1, 14);
     ScriptManager::render('primary', 1, 18);
     ScriptManager::render('primary-defer', 1, 42);
-    ScriptManager::render('prm-components', 1, 260);
+    ScriptManager::render('prm-components', 1, 261);
     //ScriptManager::render('pdfmake', 1);
     ?>
 </head>
