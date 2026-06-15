7d62304dfd8527dcd7d808ef5278f7072bacec2d
Krymengchhorng <mengchhorngk@gmail.com>
2026-06-15 11:44:34 +0700

    get

--- resources/views/layouts/prm/tenantComponent.blade.php
+++ resources/views/layouts/prm/tenantComponent.blade.php
@@ -27,18 +27,22 @@
                         </label>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 col-lg-2">
-                    <button type="button" class="w-100 w-md-auto btn-print" id="_tenant_btn_pdf">
-                        <i class="fa fa-print text-white me-2"></i>
-                        <span class="text-white" vslang="buttons.Print Tenant"></span>
-                    </button>
-                </div>
-                
-                <div class="col-12 col-md-6 col-lg-2 text-end">
-                    <button type="button" class="w-100 w-md-auto btnAddNewPrm" id="_btnAddTenant">
-                        <i class="me-2 fa fa-user-plus"></i>
-                        <span vslang="buttons.Create Tenant">Create Tenant</span>
-                    </button>
+                <div class="col-12 col-md-6 col-lg-4 ms-md-auto">
+                    <div class="row g-2 justify-content-end">
+                        <div class="col-12 col-md-auto">
+                            <button type="button" class="btn-print w-100" id="_tenant_btn_pdf">
+                                <i class="fa fa-print text-white me-2"></i>
+                                <span class="text-white" vslang="buttons.PDF"></span>
+                            </button>
+                        </div>
+
+                        <div class="col-12 col-md-auto">
+                            <button type="button" class="btnAddNewPrm w-100" id="_btnAddTenant">
+                                <i class="fa fa-user-plus me-2"></i>
+                                <span vslang="buttons.Create Tenant">Create Tenant</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
