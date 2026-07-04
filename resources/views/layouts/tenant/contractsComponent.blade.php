2d8838fe2c5cb273b4cdba86a4427f29a5540a52
krymenghuy <menghuykry@gmail.com>
2026-07-04 09:03:41 +0700

    update1

--- resources/views/layouts/tenant/contractsComponent.blade.php
+++ resources/views/layouts/tenant/contractsComponent.blade.php
@@ -38,7 +38,8 @@
 }
 
 .contract-card-grid > [class*="col-"] {
-    align-self: flex-start;
+    align-self: stretch;
+    display: flex;
 }
 
 .contract-card {
@@ -48,6 +49,7 @@
     box-shadow: 0 2px 8px rgba(17, 24, 39, 0.06);
     overflow: hidden;
     width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     transition: box-shadow 0.35s ease;
@@ -248,7 +250,6 @@
 }
 
 .contract-card__renewal-panel {
-    margin-top: 12px;
     min-height: 80px;
 }
 
