2b3e37cc6081eece7c0d341633b6f2de1ba42155
Krymengchhorng <mengchhorngk@gmail.com>
2026-07-16 09:26:14 +0700

    get

---
+++ resources/views/layouts/mhr/taxBracketComponent.blade.php
@@ -0,0 +1,12 @@
+<div id="_main_taxBracketComponent" style="display:none;padding:20px 0 0;">
+    <div class="d-flex justify-content-end w-100 shadow rounded-3 p-3" id="_divFilter">
+        <div class="d-flex align-items-center justify-content-end w-100">
+            <button type="button" class="btn_add" id="_btnAddTaxBracket">
+                <i class="fa fa-usd mr-2"></i>
+                <span>Create Tax Bracket</span>
+            </button>
+        </div>
+    </div>
+    <div id="_taxBracket_list" class="mt-3 px-3"></div>
+</div>
+
