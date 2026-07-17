270d6a3871d7cb8580eac49dd9e0847259620aac
krymenghuy <menghuykry@gmail.com>
2026-07-17 15:39:54 +0700

    update1

--- public/js/components/mhr/CheckPointCategoryComponent.js
+++ public/js/components/mhr/CheckPointCategoryComponent.js
@@ -29,9 +29,11 @@ var CheckPointCategoryComponent = (function () {
             transTitle: "titles.Last Updated",
             className: "align-middle text-nowrap",
             data: (data, index, tr) => {
-                return `<div class="d-flex flex-column" style="width:180px;">
-                    <span class="text-capitalize text-start text-prm-custom">${data.update_user ?? "-"}</span>
-                    <small class="text-muted">${data.updated_at ?? "-"}</small>
+                const [date, time] = (data.updated_at ?? "").split(" ");
+                return `<div class="d-flex flex-column align-items-center text-center" style="width:180px;">
+                    ${data.update_user ? `<span class="text-capitalize text-prm-custom">${data.update_user}</span>` : ""}
+                    <small class="text-muted">${date || "-"}</small>
+                    <small class="text-muted">${time ?? ""}</small>
                 </div>`;
             },
         },
