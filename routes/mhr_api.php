f44a521c616a04662dee481201b5387e0bceae9d
Sivminhh <sivminhkuoch@gmail.com>
2026-07-17 17:12:53 +0700

    upd-positiion

--- routes/mhr_api.php
+++ routes/mhr_api.php
@@ -26,6 +26,10 @@ use App\Http\Controllers\Mhr\WorkShiftController;
 use App\Http\Controllers\Mhr\WarningController;
 use App\Http\Controllers\Mhr\TaxBracketController;
 use App\Http\Controllers\Mhr\SkillController;
+use App\Http\Controllers\Mhr\JobLevelController;
+use App\Http\Controllers\Mhr\PositionController;
+use App\Http\Controllers\Mhr\DepartmentController;
+use App\Http\Controllers\Mhr\TaxAllowanceController;
 
 Route::middleware(['auth.api',CustomRateLimiter::class])->prefix('dashboard')->group(function () {
     Route::post('/data', [DashboardController::class, 'getDashboardData']);
