bb31b7f5f6a9e1bb4f4c6ae5f4582df2fcd3debe
Krymengchhorng <mengchhorngk@gmail.com>
2026-07-22 17:38:33 +0700

    get

--- resources/views/menus/mhr_menu.blade.php
+++ resources/views/menus/mhr_menu.blade.php
@@ -164,7 +164,7 @@ function v_display($mod_id, $module_ids = null)
                                     <span class="kt-menu__link-icon">
                                         <img class="opacity-icons icons" src="{{ asset('assets/images/icons/receipt.png') }}" />
                                     </span>
-                                    <span class="font-color kt-menu__link-text" vslang="menus.Track Shifts">Track Shifts</span>
+                                    <span class="font-color kt-menu__link-text" vslang="menus.Attendance Track">Attendance Track</span>
                                 </a>
                             </li>
                         </ul>
